394649af8f0946f03a89ee9edb3901df9a6a5fcb
ashimalaka <mutasimbilahql@gmail.com>
2026-05-19 11:34:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,21 @@ import streamlit as st
 
 st.title("🎈 aplikasi kelas c")
 import streamlit as st
+import streamlit as st
 
+st.markdown("*Streamlit* is **really** ***cool***.")
+st.markdown('''
+    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+multi = '''If you end a line with two spaces,
+a soft return is used for the next line.
+
+Two (or more) newline characters in a row will result in a hard return.
+'''
+st.markdown(multi)
 
 
 st.title("🧮 Kalkulator Sederhana")
